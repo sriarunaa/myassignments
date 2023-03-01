689b8328e34bc518adf2272c6e176f73408d4f5f
sriarunaa <126480072+sriarunaa@users.noreply.github.com>
2023-03-01 19:34:37 +0530

    Add files via upload

---
+++ Report.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+     
+    	 Student test=new Student();
+    	 test.studentName();
+    	 test.rollNo();
+    	 test.collegeName();
+    	 test.markScored();
+    	 test.cgpa();
+     }
+}
+
+
+
+	
+
+	
